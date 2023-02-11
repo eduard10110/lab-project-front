d9beccf61f840f9ac0fa0ebb02cca32081adff94
Eduard10110 <edgabrielyan777@gmail.com>
2023-02-11 23:46:53 +0400

    change db address

--- src/helpers/constants/index.js
+++ src/helpers/constants/index.js
@@ -11,7 +11,7 @@ import Utils from "helpers/utils";
 import routes from "routes/routes";
 
 export const HostUrls = {
-  BASE_URL: "https://eipq.am/ayg",
+  BASE_URL: "http://localhost:9164",
 };
 
 export const HOSTS = {
